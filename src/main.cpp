95f5ec1c72ed5e5a0eb76055c2d68a35da95239b
aceinetx <mouselin82@gmail.com>
2024-11-29 13:37:13 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/ProfilePage.hpp>
+#include <Geode/utils/web.hpp>
 
 #include <format>
+#include <json.hpp>
+
+using json = nlohmann::json;
+
+#define GITHUB_DATA_URL "https://raw.githubusercontent.com/aceinetx/EternalUtils/refs/heads/main/data.json"
 
 class $modify(EternalProfilePage, ProfilePage){
 	bool init(int accountID, bool ownProfile){
@@ -19,7 +25,28 @@ class $modify(EternalProfilePage, ProfilePage){
 
 	void getUserInfoFinished(GJUserScore* p0){
 		ProfilePage::getUserInfoFinished(p0);
-		return;
+
+		std::string rank = std::format("{} (!)", p0->m_globalRank);
+
+		web::WebRequest req = web::WebRequest();
+		auto task = req.get(GITHUB_DATA_URL);
+		while(!task.isFinished());
+		if(task.getFinishedValue()->string().isOk()){
+			std::string result = task.getFinishedValue()->string().unwrap();
+
+			json data = json::parse(result);
+			json demonlist_scores = data["demonlist_scores"];
+
+			for(int i=0; i<demonlist_scores.size(); i++){
+				std::string username = demonlist_scores[i][0].template get<std::string>();
+				int points = demonlist_scores[i][1].template get<int>();
+
+				if(username == p0->m_userName){
+					rank = std::format("{} (Demonlist: {})", p0->m_globalRank, points);
+				}
+			}
+
+		}
 
 		cocos2d::CCNode* layer = (cocos2d::CCNode*)this->getChildren()->firstObject();
 		layer->removeChildByID("global-rank-label");
@@ -28,7 +55,6 @@ class $modify(EternalProfilePage, ProfilePage){
 		rank_label_pos.x = 160.5;
 		rank_label_pos.y = 277;
 
-		std::string rank = std::format("{} (Demonlist: )", p0->m_globalRank);
 
 		auto label = CCLabelBMFont::create(rank.c_str(), "chatFont.fnt");
     layer->addChildAtPosition(label, Anchor::Center, rank_label_pos);
